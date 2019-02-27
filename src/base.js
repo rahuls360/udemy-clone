aa8a1947786a826b92f4fd28230cadae2dbcdba5
Rahul Makhija <rahuls360@gmail.com>
2019-02-27 10:46:39 +0530

    setpup firebase + import data to firebase

---
+++ src/base.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import Rebase from 're-base';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: "udemy-clone-6a28e",
+    storageBucket: "",
+    messagingSenderId: "439557186407"
+  });
+
+  const base = Rebase.createClass(firebaseApp.database());
+
+  export {firebaseApp};
+
+  export default base;
