d7f9d533c5849d674ce8b609e704a516a886e501
Rahul Makhija <rahuls360@gmail.com>
2019-02-27 12:33:25 +0530

    add modal for register and login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Navbar = () => (
+  <>
   <nav className="navbar navbar-inverse">
     <div className="container-fluid">
       <div className="navbar-header">
@@ -16,14 +17,54 @@ const Navbar = () => (
           <a href="/courses">Courses</a>
         </li>
         <li>
-          <a href="/login">Login</a>
+        <a href="#" data-toggle="modal" data-target="#login">Login</a>
         </li>
         <li>
-          <a href="/register">Register</a>
+          <a href="#" data-toggle="modal" data-target="#register">Register</a>
         </li>
       </ul>
     </div>
   </nav>
+
+<div id="register" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Create an account</h4>
+      </div>
+      <div class="modal-body">
+        <button>Sign up with Google Account</button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">X</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div id="login" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Login into your account</h4>
+      </div>
+      <div class="modal-body">
+        <button>Login with Google Account</button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+</>
 );
 
 export default Navbar;
