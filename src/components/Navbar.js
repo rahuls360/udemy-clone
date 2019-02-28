3b7af9a75cc389546fa39dc0ec5e5ba4dccd1fec
Rahul Makhija <rahuls360@gmail.com>
2019-02-28 11:49:49 +0530

    store data in variables for sign in with google

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,27 +6,19 @@ class Navbar extends React.Component{
   loginWithGoogle = () =>{
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          // var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          var userData = {
+          let user = result.user;
+          let userData = {
             displayName: user.displayName,
             photoURL: user.photoURL,
             email: user.email,
-            uid: user.uid
+            uid: user.uid,
+            authToken: result.credential.accessToken
           }
           console.log(userData);
           $('#login').modal('hide');
         }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
+          $('#login').modal('hide');
+          console.log(error);
         });
       }
   render(){
@@ -58,38 +50,38 @@ class Navbar extends React.Component{
           </div>
         </nav>
 
-        <div id="register" class="modal fade" role="dialog">
-          <div class="modal-dialog">
+        <div id="register" className="modal fade" role="dialog">
+          <div className="modal-dialog">
 
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Create an account</h4>
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h4 className="modal-title">Create an account</h4>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <button>Sign up with Google Account</button>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">X</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">X</button>
               </div>
             </div>
 
           </div>
         </div>
 
-        <div id="login" class="modal fade" role="dialog">
-          <div class="modal-dialog">
+        <div id="login" className="modal fade" role="dialog">
+          <div className="modal-dialog">
 
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Login into your account</h4>
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h4 className="modal-title">Login into your account</h4>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <button onClick={this.loginWithGoogle}>Login with Google Account</button>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
               </div>
             </div>
 
